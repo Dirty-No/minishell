cfa84965e0c41c888badb54533f65a00964a8d73
Smaccary <smaccary@student.42.fr>
2021-01-23 16:43:29 +0100

    added unit tests

--- tests/unit_test.sh
+++ tests/unit_test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+
 cmp_shell()
 {
 	FUNC_RET=0;
@@ -41,11 +43,73 @@ interactive()
 	done
 }
 
+unit_multiline()
+{
+	cmp_shell '
+/bin/echo 1
+/bin/echo 2
+/bin/echo 3
+/bin/echo 4
+/bin/echo 5
+/bin/echo 6
+/bin/echo 7
+/bin/echo 8
+/bin/echo 9
+/bin/echo 10'
+	cmp_shell '
+/bin/echo "1
+2
+3
+4
+5
+6
+7
+8
+9
+10"'
+	cmp_shell "
+/bin/echo '1
+2
+3
+4
+5
+6
+7
+8
+9
+10'"
+
+	cmp_shell "
+/bin/echo '1
+2
+3
+4
+5
+6
+7
+8
+9
+10' | grep 3"
+
+	cmp_shell "
+/bin/echo '1
+2
+bonjour
+4
+5
+6
+bonuit
+8
+9
+10' | grep bon"
+
+}
+
 unit_no_arg()
 {
 	cmp_shell 'ls'
 	cmp_shell 'pwd'
-	cmp_shell 'echo'
+	cmp_shell '/bin/echo'
 	cmp_shell 'uname'
 	cmp_shell 'arch'
 	cmp_shell './executables/no_arg.sh'
@@ -54,9 +118,51 @@ unit_no_arg()
 
 unit_arg()
 {
-	cmp_shell echo Hello World
+	cmp_shell /bin/echo Hello World
 	cmp_shell ls ..
 	cmp_shell uname -a
+}
+
+unit_parsing()
+{
+	cmp_shell 'export A=p;export B=w;$A$B"d"'
+	cmp_shell "/bin/echo '\'"
+	cmp_shell '/bin/echo \\'
+	cmp_shell '/bin/echo au feeling'
+	cmp_shell '/bin/echo "au feeling"'
+	cmp_shell '/bin/echo \$PATH'
+	cmp_shell '/bin/echo a;/bin/echo b;/bin/echo c;/bin/echo d;/bin/echo e;/bin/echo f;/bin/echo g;/bin/echo h;/bin/echo i;/bin/echo j;/bin/echo k;/bin/echo l;/bin/echo m;/bin/echo n;/bin/echo o;/bin/echo p;/bin/echo q;/bin/echo r;/bin/echo s;/bin/echo t;/bin/echo u;/bin/echo v;/bin/echo w;/bin/echo x;/bin/echo y;/bin/echo z;/bin/echo A;/bin/echo B;/bin/echo C;/bin/echo D;/bin/echo E;/bin/echo F'
+	cmp_shell '/bin/echo 0;/bin/echo 1;/bin/echo 2;/bin/echo 3;/bin/echo 4;/bin/echo 5;/bin/echo 6;/bin/echo 7;/bin/echo 8;/bin/echo 9;/bin/echo 10;/bin/echo 11;/bin/echo 12;/bin/echo 13;/bin/echo 14;/bin/echo 15;/bin/echo 16;/bin/echo 17;/bin/echo 18;/bin/echo 19;/bin/echo 20'
+}
+
+unit_pipes()
+{
+	cmp_shell 'printf "caca\nprout" | grep prout'
+	cmp_shell 'printf "caca\nprout" | grep caca'
+	cmp_shell 'printf "bonjour\nsalut\nbonuit" | grep bon'
+	cmp_shell 'printf "bonjour\nsalut\nbonuit" | grep bon | grep nuit'
+	cmp_shell 'printf "bonjour\nsalut" | grep salut | grep salut | grep salut | grep salut | grep salut | grep salut | grep salut | grep salut | grep salut | grep salut | grep salut | grep salut | grep salut | grep salut | grep salut | grep salut | grep salut | grep salut | grep salut '
+	cmp_shell 'export H=hello;/bin/echo hello | grep hello | grep $H '
+	cmp_shell 'export H=hello;/bin/echo hello | grep hello | grep $H | grep hello | grep $H | grep hello | grep $H | grep hello | grep $H | grep hello | grep $H | grep hello | grep $H | grep hello | grep $H | grep hello | grep $H | grep hello | grep $H'
+	cmp_shell '/bin/echo \| | grep \|'
+	cmp_shell '/bin/echo \|\|\|\|\| | grep \|'
+}
+
+unit_echo()
+{
+	cmp_shell 'echo'
+	cmp_shell 'echo hello'
+	cmp_shell 'echo'
+	cmp_shell 'echo -n hello'
+	cmp_shell 'echo -n hello world'
+	cmp_shell 'echo -n'
+	cmp_shell 'echo -n -n'
+	cmp_shell 'echo -n n -n'
+	cmp_shell 'echo -n -m'
+	cmp_shell 'echo -nv'
+	cmp_shell 'echo --help'
+	cmp_shell 'echo "hello world"'
+	cmp_shell 'echo "hello    " "       world" hi hey'
 }
 
 main()
@@ -79,11 +185,13 @@ main()
 		interactive;
 	else
 		for test in "$@";do
-			OUTPUT="$($test)";
-			if [ -z "$OUTPUT" ];then
-				printf '%-15s [OK]\n' "$test:";
-			else
-				printf '%-15s [KO]\n%s\n' "$test:" "$OUTPUT"
+			if ! [ "$test" = "-v" ];then
+				OUTPUT="$($test)";
+				if [ -z "$OUTPUT" ];then
+					printf '%-15s [OK]\n' "$test:";
+				else
+					printf '%-15s [KO]\n%s\n' "$test:" "$OUTPUT"
+				fi
 			fi
 		done
 	fi
